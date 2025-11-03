07a602d5748e40a9aaba6a6205f6c193ee25bbe8
rejacopyright <reja.copyright@gmail.com>
2025-11-03 13:38:44 +0700

    fix: Query Client Provider

--- src/integrations/tanstack-query/root-provider.tsx
+++ src/integrations/tanstack-query/root-provider.tsx
@@ -1,20 +1,38 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister'
+import { keepPreviousData, QueryCache, QueryClient } from '@tanstack/react-query'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
 
-export function getContext() {
-  const queryClient = new QueryClient()
-  return {
-    queryClient,
-  }
-}
+import type { ReactNode } from 'react'
 
-export function Provider({
-  children,
-  queryClient,
-}: {
-  children: React.ReactNode
-  queryClient: QueryClient
-}) {
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      placeholderData: keepPreviousData,
+      refetchOnMount: true,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      retry: false,
+      gcTime: 1000 * 60 * 60 * 12, // 12 hours
+      // staleTime: Infinity,
+      throwOnError: false,
+    },
+  },
+  queryCache: new QueryCache({
+    onError: (err: any) => {
+      const errMessage: any = err?.response?.data?.message?.reason || err?.message
+      return errMessage
+    },
+  }),
+})
+
+const persister = createAsyncStoragePersister({
+  storage: typeof window !== 'undefined' ? localStorage : null,
+})
+
+export const Provider = ({ children }: { children: ReactNode }) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}>
+      {children}
+    </PersistQueryClientProvider>
   )
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,5 @@
-import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister'
 import { TanStackDevtools } from '@tanstack/react-devtools'
-import { keepPreviousData, QueryCache, QueryClient } from '@tanstack/react-query'
-import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
+import type { QueryClient } from '@tanstack/react-query'
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 
@@ -12,34 +10,9 @@ interface MyRouterContext {
   queryClient: QueryClient
 }
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      placeholderData: keepPreviousData,
-      refetchOnMount: true,
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false,
-      retry: false,
-      gcTime: 1000 * 60 * 60 * 12, // 12 hours
-      // staleTime: Infinity,
-      throwOnError: false,
-    },
-  },
-  queryCache: new QueryCache({
-    onError: (err: any) => {
-      const errMessage: any = err?.response?.data?.message?.reason || err?.message
-      return errMessage
-    },
-  }),
-})
-
-const persister = createAsyncStoragePersister({
-  storage: typeof window !== 'undefined' ? localStorage : null,
-})
-
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
-    <PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}>
+    <>
       <Header />
       <Outlet />
       <TanStackDevtools
@@ -49,9 +22,6 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
           TanStackQueryDevtools,
         ]}
       />
-    </PersistQueryClientProvider>
+    </>
   ),
-  context: () => {
-    return { queryClient }
-  },
 })
